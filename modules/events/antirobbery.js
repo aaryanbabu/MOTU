d593f56e58f3cba2199c79365a30a2cb94958b6f
Aaryan <159102426+aaryan235@users.noreply.github.com>
2024-03-27 11:20:56 +0530

    Add files via upload

--- modules/events/antirobbery.js
+++ modules/events/antirobbery.js
@@ -2,7 +2,7 @@ module.exports.config = {
     name: "guard",
     eventType: ["log:thread-admins"],
     version: "1.0.0",
-    credits: "D-Jukie",
+    credits: "𝙋𝙧𝙞𝙮𝙖𝙣𝙨𝙝 𝙍𝙖𝙟𝙥𝙪𝙩",
     description: "Prevent admin changes",
 };
 
